f9fa095cbf841c62924590dd9ed74ed801351120
‎ <>
2025-02-08 09:04:52 +0700

    update

--- dev/test_downloader.dart
+++ dev/test_downloader.dart
@@ -7,8 +7,7 @@ void main(List<String> args) async {
   final YoutubeNoAuth youtubeNoAuth = YoutubeNoAuth();
   {
     await for (final _ in youtubeNoAuth.downloadAsAudio(
-      youtubeUrl:
-          "https://www.youtube.com/watch?v=_qDML_BCju8&list=PLP0zAgTBT331Z4vrbKb3zf0LR_9-_Dser",
+      youtubeUrl: "https://www.youtube.com/watch?v=Y5asikkcHiQ",
       directory: Directory(path.join(Directory.current.path, "download")),
     )) {}
   }
