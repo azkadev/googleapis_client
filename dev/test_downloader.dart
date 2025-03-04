fc1d92c77c782efa9315d68ef0d4bee889b1a13c
‎ <>
2025-03-05 03:40:02 +0700

    update

--- dev/test_downloader.dart
+++ dev/test_downloader.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: dead_code
+
 import 'dart:io';
 
 import 'package:googleapis_client/youtube/youtube.dart';
@@ -5,9 +7,18 @@ import "package:path/path.dart" as path;
 
 void main(List<String> args) async {
   final YoutubeNoAuth youtubeNoAuth = YoutubeNoAuth();
-  {
+  final String url = "https://www.youtube.com/watch?v=zQjgD3WlUTE";
+  bool isAudio = true;
+  if (isAudio) {
     await for (final _ in youtubeNoAuth.downloadAsAudio(
-      youtubeUrl: "https://www.youtube.com/watch?v=Y5asikkcHiQ",
+      youtubeUrl: url,
+      directory: Directory(path.join(Directory.current.path, "download")),
+    )) {
+      // 
+    }
+  } else {
+    await for (final _ in youtubeNoAuth.downloadAsVideo(
+      youtubeUrl: url,
       directory: Directory(path.join(Directory.current.path, "download")),
     )) {}
   }
